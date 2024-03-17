d7a0a9cf107706194d6dec7eadc4f1b69ade4ee7
Code-With-Nik <135944710+Code-With-Nik@users.noreply.github.com>
2024-03-17 12:47:51 +0530

    Problem2 BitMagic

--- AdvanceJava/src/BitMagic/bitMagic2.java
+++ AdvanceJava/src/BitMagic/bitMagic2.java
@@ -1,5 +1,8 @@
 package BitMagic;
 
+import java.sql.SQLOutput;
+import java.util.logging.SocketHandler;
+
 public class bitMagic2 {
     static int countSet(int x){
         int tot=0;
@@ -14,9 +17,39 @@ public class bitMagic2 {
         }
         return tot;
     }
+    static int countSet1(int n){
+        int res=0;
+        while(n>0){
+            n=(n&(n-1));//removing the last bit
+            res++;
+        }
+
+        return res;
+    }
+
+    static int[] initialize(){
+        int[] table1 =new int[256];
+        table1[0]=0;
+        for(int i=1;i<256;i++){
+            table1[i]=(i&1)+table1[i/2];
+        }
+        return table1;
+    }
+    static int countSet2(int n){
+        int [] table1= initialize();
+        int res=table1[n&0xff];/*0xff is basically 11111111*/
+        n=n>>8;
+        res=res+table1[n&0xff];
+        n=n>>8;
+        res=res+table1[n&0xff];
+        n=n>>8;
+        res=res+table1[n&0xff];
+        return res;
+    }
     public static void main(String[] args) {
         System.out.println(countSet(7));
-        System.out.println(countSet(9));
+        System.out.println(countSet1(9));
+        System.out.println(countSet2(11));
 
     }
 }
