240acc368d5d9dd56914debdedac8339e8f1e399
Code-With-Nik <135944710+Code-With-Nik@users.noreply.github.com>
2024-03-19 10:28:08 +0530

    Add

---
+++ AdvanceJava/src/BitMagic/bitMagic6.java
@@ -0,0 +1,35 @@
+package BitMagic;
+
+public class bitMagic6 {
+    static void findA(int []array,int n){
+        int xor=0;
+        int res1=0;
+        int res2=0;
+
+        for(int i=0;i<n;i++){
+            xor=xor^array[i];
+
+
+        }
+        int rm=xor&~(xor-1);
+        for(int i=0;i<n;i++){
+            if((array[i]&rm)!=0){
+                res1=res1^array[i];
+            }
+            else{
+                res2=res2^array[i];
+            }
+
+        }
+        System.out.println(res1+" "+res2);
+
+    }
+    public static void main(String[] args) {
+        int []array={1,2,2,3,4,5,4,6,6,5};
+        findA(array,10);
+
+
+
+
+    }
+}
